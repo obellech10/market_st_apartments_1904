700893703ab4e07e0e2e55160d8889db6cc474c4
Oscar Bellechasse <obellech10@gmail.com>
2019-05-15 09:30:54 -0600

    Create building and building_test with initialize tests

---
+++ test/building_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/apartment'
+require './lib/renter'
+require './lib/building'
+
+class BuildingTest < MiniTest::Test
+
+  def setup
+    @building = Building.new
+    @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @b2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2}
+  end
+
+  def test_that_it_exists
+    assert_instance_of Building, @building
+  end
+
+  def test_that_it_has_attributes
+    assert_equal [], @building.units
+  end
+
+end
