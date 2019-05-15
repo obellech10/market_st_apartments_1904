0c9bd7d282e4afea9f58de4b42cdf96355799a84
Oscar Bellechasse <obellech10@gmail.com>
2019-05-15 09:45:39 -0600

    Add average_rent method to building

--- lib/building.rb
+++ lib/building.rb
@@ -5,8 +5,16 @@ class Building
     @units = []
   end
 
-    def add_unit(apartment)
-      @units << apartment
+  def add_unit(apartment)
+    @units << apartment
+  end
+
+  def average_rent
+    total_rent = 0
+    @units.each do |apt|
+      total_rent += apt.monthly_rent
     end
+    total_rent.to_f / @units.count
+  end
 
 end
