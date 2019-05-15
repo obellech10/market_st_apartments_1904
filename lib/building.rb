1675d6916d2f1c286e7d79aa7b4751c10104d67c
Oscar Bellechasse <obellech10@gmail.com>
2019-05-15 10:38:07 -0600

    Add method for renter with highest rent

--- lib/building.rb
+++ lib/building.rb
@@ -17,4 +17,20 @@ class Building
     total_rent.to_f / @units.count
   end
 
+  # This is very ugly but couldn't figure out a better way
+  def renter_with_highest_rent
+    all_renters = []
+    @units.select do |unit|
+      if unit.renter != nil
+        all_renters << unit
+      end
+    end
+
+    highest_rent = all_renters.max_by do |unit|
+           unit.monthly_rent
+         end
+    highest_rent.renter
+  end
+
+
 end
