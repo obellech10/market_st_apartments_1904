ee12a800af602e8779f6e78e66fcbbe95f2c732c
Oscar Bellechasse <obellech10@gmail.com>
2019-05-15 09:09:40 -0600

    Created renter file and renter_test file

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -1,6 +1,19 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/renter'
+require 'pry'
 
 class RenterTest < Minitest::Test
+
+  def setup
+    @jessie = Renter.new("Jessie")
+  end
+
+  def test_that_it_exists
+    assert_instance_of Renter, @jessie
+  end
+
+  def test_that_it_has_attributes
+    assert_equal "Jessie", @jessie.name
+  end
 end
