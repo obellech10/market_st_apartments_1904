1a83b77562e63dec54f6512124672d22ba03c846
Oscar Bellechasse <obellech10@gmail.com>
2019-05-15 09:17:43 -0600

    Create apartment and apartment_test files with initialize tests

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,2 +1,12 @@
 class Apartment
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms
+
+  def initialize(number:, monthly_rent:, bathrooms:, bedrooms:)
+    @number = number
+    @monthly_rent = monthly_rent
+    @bathrooms = bathrooms
+    @bedrooms = bedrooms
+  end
+
+
 end
--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -4,4 +4,20 @@ require './lib/apartment'
 require './lib/renter'
 
 class ApartmentTest < Minitest::Test
+
+  def setup
+    @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @jessie = Renter.new("Jessie")
+  end
+
+  def test_that_it_exists
+    assert_instance_of Apartment, @a1
+  end
+
+  def test_that_it_has_attributes
+    assert_equal "A1", @a1.number
+    assert_equal 1200, @a1.monthly_rent
+    assert_equal 1, @a1.bathrooms
+    assert_equal 1, @a1.bedrooms
+  end
 end
