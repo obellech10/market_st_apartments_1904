aa565a88788cc19ba6d9a98acdcdb146db3a5612
Oscar Bellechasse <obellech10@gmail.com>
2019-05-15 09:37:57 -0600

    Create test for adding units to building

--- test/building_test.rb
+++ test/building_test.rb
@@ -9,7 +9,7 @@ class BuildingTest < MiniTest::Test
   def setup
     @building = Building.new
     @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-    @b2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2}
+    @b2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
   end
 
   def test_that_it_exists
@@ -19,5 +19,13 @@ class BuildingTest < MiniTest::Test
   def test_that_it_has_attributes
     assert_equal [], @building.units
   end
+
+  def test_that_it_has_units
+    @building.add_unit(@a1)
+    @building.add_unit(@b2)
+
+    assert_equal [@a1, @b2], @building.units
+  end
+  
 
 end
